b73a5ce9bc2b8ccd05015da36acf517bbddd9ce0
Dmitrykhomanko <stalker400@mail.ru>
2023-06-18 19:10:18 +0300

    122 123 were comitted and 124

--- dimka_food/dimka_food/settings.py
+++ dimka_food/dimka_food/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
